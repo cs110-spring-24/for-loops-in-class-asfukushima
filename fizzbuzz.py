88e9e40153f189f34318ac89218ab28b6117399b
asfukushima <157657940+asfukushima@users.noreply.github.com>
2024-02-12 16:13:41 -0800

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+for count in range(1, 31):
+    if count % 15 == 0:
+        print("fizz buzz")
+    elif count % 3 == 0:
+        print("fizz")
+    elif count % 5 == 0:
+        print("buzz")
+    else:
+        print(count)
+    
+# print out all numbers between 1-30
+# if the number is a multiple of 3, print fizz
+# if the number is a multiples of 5, print buzz
+# if the number is a multiple of 15, print fizzbuzz
+        # the order of your conditions matter
